51f1524f5cb474a6203924cf1f7855cb4fc128dd
gmoon1117 <30319741+gmoon1117@users.noreply.github.com>
2019-03-03 16:40:56 -0500

    hi

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -10,9 +10,6 @@ class Message extends React.Component {
         <div className='username'>
           { this.props.username }
         </div>
-        <div className ='bubble'>
-        {this.props.username[0].toUpperCase()}
-        </div>
         <div className='message-body'>
           { this.props.message }
         </div>
